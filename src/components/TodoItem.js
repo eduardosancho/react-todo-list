5dd9654fef38e3278dcb35341382e8ddc57c0ea2
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-15 02:00:30 -0600

    Add working delete button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,6 +9,9 @@ export default class TodoItem extends Component {
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
         {this.props.todo.title}
       </li>
     )
