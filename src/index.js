61c1f736325f570ae2258d901bd20de9d74c3046
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-02-15 03:27:28 -0600

    Cant believe this was broken

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 // Component file
 import TodoContainer from './components/TodoContainer';
 
-//stylesheeeeet
-import "./App.css"
+// stylesheeeeet
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
